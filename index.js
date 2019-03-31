c3896fcbb019f82f76811b2ca792846d0b5dc1bb
Abagail Hudson <abagailanne@gmail.com>
2019-03-31 18:06:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// add solution here
+
+function johnLennonFacts(){
+
+  return array
+}
+
+var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+function iLoveTheBeatles(num){
+var arrayNew = []
+
+if(num !== 17){
+  do{
+      arrayNew.push("I love the Beatles!")
+    }
+  while (incrementVariable() <= num)
+    }
+else{
+    arrayNew.push("I love the Beatles!")
+    }
+return arrayNew
+}
